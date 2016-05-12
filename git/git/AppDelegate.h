046bff1466e95e6c02f10306ba00b8d5af4ac3aa
Leerq <leerq@leerqdemacbook-pro.local>
2016-05-12 20:01:51 +0800

    new

---
+++ git/git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by Leerq on 16/5/12.
+//  Copyright © 2016年 SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
