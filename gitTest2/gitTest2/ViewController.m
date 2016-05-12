cf1a5ffdb697f7b3f82be1a10d56cd06627421ac
Leerq <leerq@leerqdemacbook-pro.local>
2016-05-12 19:45:47 +0800

    new

---
+++ gitTest2/gitTest2/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  gitTest2
+//
+//  Created by Leerq on 16/5/12.
+//  Copyright © 2016年 SL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)test1 {
+    
+    
+    NSLog(@"Hello world");
+}
+
+@end
---
+++ gitTest2/gitTest2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitTest2
+//
+//  Created by Leerq on 16/5/12.
+//  Copyright © 2016年 SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
